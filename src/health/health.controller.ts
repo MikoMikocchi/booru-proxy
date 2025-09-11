7446db4a73d165700bbbfffd76eefd9e7cb29b05
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2025-09-11 17:17:09 +0300

    chore(deps): update @nestjs/platform-express and add @nestjs/terminus

---
+++ src/health/health.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common'
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus'
+import { RedisHealthIndicator } from './redis.health'
+
+@Controller('health')
+export class HealthController {
+	constructor(private readonly health: HealthCheckService, private readonly redisHealthIndicator: RedisHealthIndicator) {}
+
+	@Get()
+	@HealthCheck()
+	async check() {
+		return this.health.check([
+			() => this.redisHealthIndicator.isHealthy('redis'),
+		])
+	}
+}
---
+++ src/health/redis.health.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common'
+import { HealthIndicatorService, HealthIndicatorResult } from '@nestjs/terminus'
+import Redis from 'ioredis'
+
+@Injectable()
+export class RedisHealthIndicator {
+  constructor(private readonly healthIndicatorService: HealthIndicatorService) {}
+
+	async isHealthy(key: string): Promise<HealthIndicatorResult> {
+		const indicator = this.healthIndicatorService.check(key);
+		const redis = new Redis({ host: 'localhost', port: 6379 })
+		try {
+			await redis.ping()
+			return indicator.up();
+		} catch (e) {
+			return indicator.down('Redis not available');
+		} finally {
+      await redis.disconnect();
+    }
+	}
+}
