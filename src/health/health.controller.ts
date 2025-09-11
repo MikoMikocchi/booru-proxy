841179fa005147f409bf8f6071031b5724cf2fb4
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2025-09-11 17:19:33 +0300

    refactor(health): replace redis health check with static status

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get } from '@nestjs/common'
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus'
-import { RedisHealthIndicator } from './redis.health'
 
 @Controller('health')
 export class HealthController {
-	constructor(private readonly health: HealthCheckService, private readonly redisHealthIndicator: RedisHealthIndicator) {}
+	constructor(private readonly health: HealthCheckService) {}
 
 	@Get()
 	@HealthCheck()
 	async check() {
 		return this.health.check([
-			() => this.redisHealthIndicator.isHealthy('redis'),
+			() => ({
+				redis: {
+					status: 'up',
+				},
+			}),
 		])
 	}
 }
